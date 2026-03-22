5b98836fb81a1234819ae82545bde37d54a046b9
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-23 01:00:11 +0530

    Update login.jsx

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -15,7 +15,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://food-tracking-backend.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
