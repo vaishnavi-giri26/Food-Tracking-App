8c705b6621e2f54d3e4bb02642b68c700f3111d4
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-23 01:01:24 +0530

    Update registration.jsx

--- frontend/src/components/registration.jsx
+++ frontend/src/components/registration.jsx
@@ -16,7 +16,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch("http://food-tracking-backend.onrender.com/api/auth/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
